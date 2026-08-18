db6e096361826a79fc52d37628fe01e895391668
Vishnuvardhan Pushpala <vishnuvardhanpushpala92@gmail.com>
2026-08-18 10:31:51 +0530

    Add missing RequestParam import to AdminZoneController

--- src/main/java/com/gonaturefarms/controller/AdminZoneController.java
+++ src/main/java/com/gonaturefarms/controller/AdminZoneController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam; // <--- Added this missing import
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gonaturefarms.dto.admin.ZoneRequest;
